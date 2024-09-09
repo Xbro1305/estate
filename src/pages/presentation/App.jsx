409388237e27794da8ba927576e9b8b856aab1fe
xbro1305 <x.bro2010@gmail.com>
2024-09-09 18:50:27 +0500

    added api

--- src/pages/presentation/App.jsx
+++ src/pages/presentation/App.jsx
@@ -2,49 +2,48 @@ import React, { useState } from "react";
 import "./App.css";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import img from "../../images/photo_2024-09-04_20-35-52.jpg";
-import titleImg from "../../images/titleimg.jpg";
 
 export const App = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const api = process.env.REACT_APP_API_URL;
-  const [res, setRes] = useState({
-    title: "1-BR Marina Living",
-    date: "2021-07-01",
-    images: [img, img, img, img, img, img],
-    titleImg,
-  });
+  const api = process.env.REACT_APP_BASE_URL;
+  const [res, setRes] = useState();
 
-  // axios(api + id)
-  //   .then((response) => {
-  //     if (response.status === 200) {
-  //       setRes(response.data);
-  //     }
-  //   })
-  //   .catch((err) => navigate("/404"));
+  axios(api + id, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    presentation_id: id,
+  }).then((response) => {
+    setRes(response.data);
+    console.log(response);
+  });
+  // .catch((err) => navigate("/404"));
 
   return (
-    <div className="main">
-      <img src={res.titleImg} className="titleImg" alt={res.title} />
-      <div className="title">
-        <h1>{res.title}</h1>
-        <h2>
-          <ul>
-            <li>{res.date}</li>
-          </ul>
-        </h2>
+    res && (
+      <div className="main">
+        {/* <img src={res.titleImg} className="titleImg" alt={res.title} /> */}
+        <div className="title">
+          <h1>{res.title}</h1>
+          <h2>
+            <ul>
+              <li>{res.created_at_str}</li>
+            </ul>
+          </h2>
+        </div>
+        <div className="images">
+          {res.photo_urls.map((img, i) => (
+            <figure
+              className="imgWrapper"
+              style={{ backgroundImage: `url(${img})` }}
+            >
+              <img key={i} src={img} alt={res.title} />
+            </figure>
+          ))}
+        </div>
       </div>
-      <div className="images">
-        {res.images.map((img, i) => (
-          <figure
-            className="imgWrapper"
-            style={{ backgroundImage: `url(${img})` }}
-          >
-            <img key={i} src={img} alt={res.title} />
-          </figure>
-        ))}
-      </div>
-    </div>
+    )
   );
 };
