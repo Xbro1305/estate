30b63adc217534e4b56e2bcecf7455818eeb6ff3
xbro1305 <x.bro2010@gmail.com>
2024-09-09 17:33:51 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./pages/presentation/App";
+import { NotFound } from "./pages/404/404";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.Fragment>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/presentation/:id" element={<App />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.Fragment>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/404/404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./404.css";
+
+export const NotFound = () => {
+  return (
+    <div className="notFound">
+      <h1>404</h1>
+      <p>
+        Страница не найдена <span>😢</span>
+      </p>
+    </div>
+  );
+};
